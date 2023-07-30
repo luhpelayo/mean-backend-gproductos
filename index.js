ef84594bd71121da786e04c6f1469c377109d2f8
luhpelayo <luhpelayo@gmail.com>
2023-07-30 18:25:55 -0400

    LP/conexionMongodb

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('Hola mundo');
+const express = require('express');
+const conectarDB = require('./config/db');
+const cors = require("cors");
+
+// Creamos el servidor
+const app = express();
+
+// Conectamos a la BD
+conectarDB();
+app.use(cors())
+
+app.use(express.json());
+
+//app.use('/api/productos', require('./routes/producto'));
+
+app.listen(4000, () => {
+    console.log('El servidor esta corriendo perfectamente')
+})
